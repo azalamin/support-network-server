08a49dc5e1c438479145aba718da76a4a192a2a0
Al-amin Sheikh <azalamins@gmail.com>
2022-04-27 23:17:38 +0600

    added activity to db

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+const app = express();
+
+// use middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.coipt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const activityCollection = client.db("supportNetwork").collection("activities");
+
+    // get all activities data
+    app.get("/activity", async(req, res) => {
+        const query = {};
+         const cursor = activityCollection.find(query);
+         const activities = await cursor.toArray();
+         res.send(activities)
+    });
+
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Support Network Server is running");
+});
+
+app.listen(port, () => {
+  console.log("Listening to the port", port);
+});
